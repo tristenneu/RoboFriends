589fd5c59c1349d881ffedd79705611ef785e08d
Tristen Neu <tristenneu@Tristens-iMac.local>
2018-12-22 14:48:44 -0600

    changed font from .tff to .woff

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-import './index.css';
+import 'tachyons';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { searchRobots, requestRobots } from './reducers';
-import 'tachyons';
+import './index.css';
+
 
 const logger = createLogger();
 
